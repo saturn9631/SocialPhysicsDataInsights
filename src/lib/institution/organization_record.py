3f7899f210b4475d6ea6f64cad7fe4d2f7ecf0c0
Shakari Wade <saturn.kari.9@gmail.com>
2025-04-01 11:43:08 -0700

    Changed the code to organization_record.

--- src/lib/institution/organization_record.py
+++ src/lib/institution/organization_record.py
@@ -5,17 +5,7 @@ import networkx as nx
 from data.actor import Actor
 
 class Organization(Actor):
-    def __init__(self, name, constituents):
-        __super__(self, name)
-        self.sub_orgs = []
-        self.members = []
-        self.leaders = []
-        for constituent in constituents:
-            if type(constituent) == Organization:
-                self.sub_orgs.append(constituent)
-            else:
-                self.members.append(constituent)
-                if constituent["Leader"] == True:
-                    self.leaders.append(constituent)
-
-
+    def __init__(self, data):
+        if type(data) == pd.DataFrame:
+            for index, entry in data.iterrows():
+                
