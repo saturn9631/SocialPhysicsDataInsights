ab759b95a77a5cbc9154c08d468826b70f02fbd7
Shakari Wade <saturn.kari.9@gmail.com>
2025-04-29 12:21:38 -0700

    Added data types to distinguish between dataframes containing different data.

---
+++ src/lib/data/data_type.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+import typing
+
+class Data_Type:
+    def __init__(dataframe):
+        self.data = dataframe
+class Raw_Data(Data_Type):
+
+class Candidate_Data(Data_Type):
+
+class Influencers(Data_Type):
+
+class Legislator(Data_Type):
