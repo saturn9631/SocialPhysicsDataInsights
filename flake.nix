b2318fe822ef0300049b3f25e3b98c1aa54d8f49
Shakari Wade <saturn.kari.9@gmail.com>
2025-05-19 17:21:28 -0700

    Putting flake.nix into the project since nix shell will not work without doing so.

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+	description = "A very basic flake";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-25.05-darwin";
+	};
+
+	outputs = { self, nixpkgs }: 
+	let
+			system = "x86_64-linux";
+			pkgs = nixpkgs.legacyPackages.${system};
+	in 
+	{
+			devShells.${system}.default = 
+				pkgs.mkShell {
+					buildInputs = with pkgs; [
+						jupyter-all
+						python313Full
+						python313Packages.cython
+						python313Packages.pycuda
+						python313Packages.ipython
+						python313Packages.numpy
+						python313Packages.pandas
+						python313Packages.scipy
+						python313Packages.sympy
+						python313Packages.networkx
+						python313Packages.torch
+						python313Packages.matplotlib
+						python313Packages.transformers
+					];
+				};
+	};
+}
