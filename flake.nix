56235c8276f09caa44a974a729e1c8fccaaaaafe
Shakari Wade <saturn.kari.9@gmail.com>
2025-06-07 11:51:21 -0700

    Changed nix flake to allow unfree packages.

--- flake.nix
+++ flake.nix
@@ -8,7 +8,8 @@
 	outputs = { self, nixpkgs }: 
 	let
 			system = "x86_64-linux";
-			pkgs = nixpkgs.legacyPackages.${system};
+			#pkgs = nixpkgs.legacyPackages.${system};
+			pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
 	in 
 	{
 			devShells.${system}.default = 
